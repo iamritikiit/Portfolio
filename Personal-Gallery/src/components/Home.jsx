a2cf6ae44b875735cb5cba914362b5cdf55ff9a2
Ritik Kumar <iamritikiit@gmail.com>
2024-08-12 22:33:06 +0530

    added contents into the Home page

--- Personal-Gallery/src/components/Home.jsx
+++ Personal-Gallery/src/components/Home.jsx
@@ -3,7 +3,11 @@ import React from "react"
 export default function Home() {
     return (
         <>
-            
+            <h1>Ritik Kumar</h1>
+            <h2>Welcome to My Creative Haven!</h2>
+            <p>
+                I’m Ritik, and I’m thrilled you’ve stopped by. My mission is to infuse technology with a sprinkle of creativity to build web and software solutions that shine. Here, you’ll find a selection of projects that I’m pleased to share, each reflecting my dedication and passion for what I do.
+            </p>
         </>
     )
 }
