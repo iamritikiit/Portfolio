f25b7d5cca6232c299a17d1bd5a9eab6df24b765
Ritik Kumar <iamritikiit@gmail.com>
2024-09-20 16:55:52 +0530

    modified the stylings

--- Personal-Gallery/src/App.jsx
+++ Personal-Gallery/src/App.jsx
@@ -1,21 +1,42 @@
-import { useState } from 'react'
-import './App.css'
-import Home from "../src/components/Home.jsx"
-import About from "../src/components/About.jsx"
-import Contact from "../src/components/Contact.jsx"
-import Projects from "../src/components/Projects.jsx"
-
-
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import './App.css';
+import Home from "../src/components/Home.jsx";
+import About from "../src/components/About.jsx";
+import Contact from "../src/components/Contact.jsx";
+import Projects from "../src/components/Projects.jsx";
 
 function App() {
   return (
-    <>
-      <Home />
-      <Contact />
-      <Projects />
-      <About />
-    </>
-  )
+    <Router>
+      <div className="app-container">
+        {/* Navigation Bar */}
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Routes: Pages for each link */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
