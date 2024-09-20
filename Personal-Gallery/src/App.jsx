fc3b4ebe24802dffcabb3f02abb181645e009686
Ritik Kumar <iamritikiit@gmail.com>
2024-09-20 14:09:22 +0530

    did some modification to the project and app file

--- Personal-Gallery/src/App.jsx
+++ Personal-Gallery/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Home from "../src/components/Home.jsx"
+import About from "../src/components/About.jsx"
+import Contact from "../src/components/Contact.jsx"
+import Projects from "../src/components/Projects.jsx"
+
 
 
 function App() {
   return (
     <>
       <Home />
+      <Contact />
+      <Projects />
+      <About />
     </>
   )
 }
--- Personal-Gallery/src/components/Projects.jsx
+++ Personal-Gallery/src/components/Projects.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import './Project.css'
 
 export default function Projects() {
   const projects = [
